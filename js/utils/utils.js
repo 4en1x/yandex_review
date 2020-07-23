043747e23d0047e5fe33eba8959c6a09e257cc98
4en1x <free_playerq@mail.ru>
2020-07-24 00:24:24 +0300

    Updates

--- js/utils/utils.js
+++
@@ -1,9 +0,0 @@
-import { ESC } from './constants';
-
-export const isEscButton = e => e.which === ESC;
-
-export const getElement = (selector, element = document) => element.querySelector(selector);
-
-export const getElements = (selector, element = document) => element.querySelectorAll(selector);
-
-export const getImageAlt = link => `Изображение ${link}`;
